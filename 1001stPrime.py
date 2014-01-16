c0e0bbb5bc8a848d51fae10f04bf5778d6529d25
Richard Bennett <rmb209@ic.ac.uk>
2014-01-16 15:47:22 +0000

    Problem 7 solution

--- 1001stPrime.py
+++ 1001stPrime.py
@@ -1,4 +1,5 @@
-
+import math
+from time import time
 
 def primeChecker(value):
 	for divisor in range(2, int(math.floor(math.sqrt(value))+1)):
@@ -9,17 +10,24 @@ def primeChecker(value):
 def nthPrime(nthValue):
 	prime = 0
 	currentValue = 0
-	for value in range(1, 999999999):
+	for value in range(1, 9999999):
 		if primeChecker(value):
-			prime = primeChecker(value)
+			prime = value
 			currentValue+=1
 			print currentValue
-			if nthValue == currentValue :
-	 			return prime;
+			if nthValue == currentValue-1 :
+				print "This is the nth prime ", prime 
+	 			return prime
 
 	
 
 
 
 if __name__ == '__main__':
-	main()
+	if not nthPrime(6) == 13:
+		raise AssertionError("Fail")
+	start = time()
+	print nthPrime(10001)
+	finish = time()
+	timetaken = finish - start
+	print timetaken
